c5bd58d294a4aa4b9f95e8baf91f380a3753b277
patrick van der tuin <patricktuin@planet.nl>
2014-04-08 22:41:25 +0200

    macbook

--- mysite/urls.py
+++ mysite/urls.py
@@ -5,7 +5,7 @@ admin.autodiscover()
 
 urlpatterns = patterns('',
 	url(r'^food/', include('food.urls', namespace="food")),
-	url(r'^wedding/', include('wedding.urls', namespace="wedding")),
-    #url(r'^polls/', include('polls.urls', namespace="polls")),
+	#url(r'^wedding/', include('wedding.urls', namespace="wedding")),
+    url(r'^polls/', include('polls.urls', namespace="polls")),
     url(r'^admin/', include(admin.site.urls)),
 )
--- wedding/views.py
+++ wedding/views.py
@@ -6,11 +6,19 @@ from django.template import RequestContext, loader
 # Create your views here.
 from wedding.models import Invitee, Invitee_extra
 
-def index(request):
+class IndexView(generic.ListView):
+    template_name = 'wedding/index.html'
+    context_object_name = 'party_list'
+
+    def get_queryset(self):
+        """Return the party names"""
+        return Invitee.objects.all().order_by('-party_name')[:5]
+
+class DetailView(generic.DetailView):
+    model = Invitee_extra
+    template_name = 'wedding/detail.html'
+
+def results(request, invitee_id):
     party_list = Invitee.objects.all().order_by('-party_name')[:5]
     context = {'party_list': party_list}
     return render(request, 'wedding/index.html', context)
-
-def detail(request, invitee_id):
-    question = get_object_or_404(Invitee, pk=invitee_id)
-    return render(request, 'wedding/detail.html', {'party_name': party_name})
